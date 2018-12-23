82a59689ec9c92417184ec75395d36899e1366d1
Monika Maria <monika.kocot9@gmail.com>
2018-12-23 13:49:58 +0100

    zad1

---
+++ src/com/company/Zad1.java
@@ -0,0 +1,31 @@
+package com.company;
+
+/* Given a table elements with the following structure:
+
+create table elements (
+v integer not null
+);
+
+write an SQL query that returns the sum of the numbers in column V:.
+For example:
+
+v
+--
+2
+10
+20
+10
+
+Your query should return 42.
+* */
+
+
+public class Zad1 {
+
+/*  SOLUTION (write it with for example MySQL Workbench):
+
+SUM(v)
+FROM elements;
+
+*/
+}
